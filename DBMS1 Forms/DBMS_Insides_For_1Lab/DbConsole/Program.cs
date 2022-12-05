adf8c1b709e57c83ab413327d5a21a9a74621cb5
DESKTOP-NVJTH5L\Alex <alexanderbor0134@mail.ru>
2022-12-05 22:54:05 +0300

    transact complete

--- DBMS1 Forms/DBMS_Insides_For_1Lab/DbConsole/Program.cs
+++ DBMS1 Forms/DBMS_Insides_For_1Lab/DbConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,43 @@ namespace DbConsole
         {
             IDb db = new Db();
             var mech = db.GetMech();
+            Transact();
+
             PrintMech(mech);
 
             Console.ReadKey();
+        }
+
+        private static void Transact()
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-NVJTH5L\\SQLEXPRESS;Initial Catalog=Lab1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+
+                SqlCommand command = connection.CreateCommand();
+                command.Transaction = transaction;
+
+                try
+                {
+                    // выполняем две отдельные команды
+                    command.CommandText = "INSERT INTO Mecha (Model, ArmoreType, WeaponType, EngineType, Type, SerialID)" +
+                        " VALUES('Игла-5', 'Полисталь', 'Фазмаган', 'Фазовый', 'Страйдер', 17)";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO Mecha (Model, ArmoreType, WeaponType, EngineType, Type, SerialID)" +
+                        " VALUES('АААААА', 'ААААА', 'АААА', 'АААА', 'АААА', 17)";
+                    command.ExecuteNonQuery();
+
+                    // подтверждаем транзакцию
+                    transaction.Commit();
+                    Console.WriteLine("Данные добавлены в базу данных");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    transaction.Rollback();
+                }
+            }
         }
 
         private static void PrintMech(List<Mech> mech)
